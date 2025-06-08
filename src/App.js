ddc58edf2d6009bbc91778c05fdcd66695ecabb2
Priyanshu Maurya <satyasaadhika.aham@gmail.com>
2025-06-08 17:06:19 +0530

    Add all pages in app.js file

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Certificates from './pages/Certificates/Certificates';
+import Projects from './pages/Projects/Projects';
+import Resume from './pages/Resume/Resume';
+import Contact from './pages/Contact/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Certificates" element={<Certificates />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Resume" element={<Resume />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
